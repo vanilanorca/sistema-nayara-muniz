815ff4c22e948552e1b4d1276077ad1115c94b60
vanilanorca <vilopesalves@hotmail.com>
2021-12-05 23:31:29 -0300

    loading

--- cron.js
+++ cron.js
@@ -5,8 +5,8 @@ function load (){
     document.getElementById("loading").style.display = "none";
     document.getElementById("conteudo").style.display = "inherit";
 }
-window.onload = load()
-// window.onload = (function () { window.setInterval('load()',2000);})
+
+window.onload = (function () { window.setInterval('load()',2000);})
 
 // capturar valor da sessao
 var form = document.getElementById('form');
